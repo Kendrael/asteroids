d1286cfdf888ba90d14931a9ecb7269e9a29a658
Kendrael <197850627+Kendrael@users.noreply.github.com>
2025-04-19 23:10:34 -0400

    Add pygame boilerplate code and requirements

---
+++ main.py
@@ -0,0 +1,11 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+# don'f forget to use "source venv/bin/activate" in terminal!
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+
+if __name__ == "__main__":
+    main()
