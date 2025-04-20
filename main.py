d8730ee8f4ebec273397a933044bbc4d561ac51c
Kendrael <197850627+Kendrael@users.noreply.github.com>
2025-04-19 23:47:18 -0400

    Add module imports and constants for Asteroids game

--- main.py
+++ main.py
@@ -3,9 +3,12 @@
 # throughout this file
 # don'f forget to use "source venv/bin/activate" in terminal!
 import pygame
+from constants import *
 
 def main():
     print("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
 
 if __name__ == "__main__":
     main()
